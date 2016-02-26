5d146078ff017b022d65a0ffffbadb7fcbbb3668
Nathan Brown <nbrown@IT472.mr.preble.com>
2016-02-26 09:13:51 -0500

    Title Screen
    Includes the transitions scripts. Quit only works in a built game.

---
+++ Assets/Standard Assets/Scripts/TitleScreenPlay.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenPlay : MonoBehaviour {
+
+	public void Play()
+    {
+        Application.LoadLevel(1); //Loads the listed level, change the number to scene needed
+    }
+}
