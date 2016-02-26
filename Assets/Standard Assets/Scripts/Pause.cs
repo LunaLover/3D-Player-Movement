def8d3de9931f84e1cdc363bef46b613b99976fd
Austin Fink <afink@utc4me.org>
2016-02-26 08:49:44 -0500

    The Game
    new repo..

---
+++ Assets/Standard Assets/Scripts/Pause.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	void FixedUpdate ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Screen.lockCursor = false;
+            Time.timeScale = 0;
+        }
+	}
+
+    void UnPause()
+    {
+        if (Time.timeScale = 0f, && Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+}
