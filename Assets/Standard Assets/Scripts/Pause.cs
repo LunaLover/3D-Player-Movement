f8e278b521d9958b450fc5fd27b12e0d6fad08ea
Nathaniel Wilcox <nwilcox@utc4me.org>
2016-03-07 10:09:37 -0500

    Revert "stuff to be reverted"
    This reverts commit a951e6cac4efe854c4e65ac13f1b9ed818b8a979.

--- Assets/Standard Assets/Scripts/Pause.cs
+++ Assets/Standard Assets/Scripts/Pause.cs
@@ -14,7 +14,7 @@ public class Pause : MonoBehaviour {
 
     void UnPause()
     {
-//        if ((Time.timeScale = 0f) && (Input.GetKeyDown(KeyCode.Escape)))
+        if (Time.timeScale = 0f, && Input.GetKeyDown(KeyCode.Escape))
         {
 
         }
