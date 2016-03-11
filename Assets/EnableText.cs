11570e600a20d48d4e5db658223ffcf9c974a44a
Nathaniel Wilcox <nwilcox@utc4me.org>
2016-03-11 09:14:10 -0500

    Bit of tinkering in the level.

---
+++ Assets/EnableText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+
+public class EnableText : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other) {
+		TextMesh mesh = (TextMesh)FindObjectOfType (typeof(TextMesh));
+		if (mesh)
+			Debug.Log ("Found the Mesh: " + mesh.name);
+		else
+			Debug.Log ("Didn't find it.");
+		mesh.gameObject.GetComponent("MeshRenderer");
+
+	}
+}
