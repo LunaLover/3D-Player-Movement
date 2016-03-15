ddf34e1b8dba703b00c43e0cad63c7dfa14a5672
Nathan Brown <nbrown@mr.preble.com>
2016-03-15 09:53:21 -0400

    Fix
    ^^^

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+    void Update()
+    {
+        bool pressed = false;
+
+        if (Input.GetButtonDown("Escape") && (pressed == false))
+        {
+            Debug.Log("Pause");
+            Time.timeScale = 0;
+            pressed = true;
+            if (pressed == true)
+            {
+                
+            }
+        }
+
+        if (Input.GetButtonDown("Escape") && (pressed == true))
+        {
+            Debug.Log("Unpause");
+            Time.timeScale = 1;
+            pressed = false;
+        }
+
+    }
+
+}
