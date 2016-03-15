e17fa431aa13dc18cbdc630d76de06bd4eb1709c
Nathaniel Wilcox <nwilcox@utc4me.org>
2016-03-15 08:57:07 -0400

    Otherstuff from the prefabbed level

---
+++ Assets/Nate W.'s stuff/ComponentDisable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComponentDisable : MonoBehaviour
+{
+	private TextMesh myMesh;
+	private GameObject wrong;
+	void Start()
+	{
+		//		var notThis = FindObjectOfType(typeof(TextMesh));
+		//		if (notThis != null)
+		//			Debug.Log (notThis + " has been named.");
+		//		var notHere = FindObjectOfType (typeof(TextMesh));
+		//		if (notHere != null)
+		//			Debug.Log (notHere + " is a GameObject.");
+		wrong = GameObject.Find ("WrongWay");
+		myMesh = wrong.GetComponent<TextMesh> ();
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		myMesh.gameObject.SetActive (false);
+	}
+}
