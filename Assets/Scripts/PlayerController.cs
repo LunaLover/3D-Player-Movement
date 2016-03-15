45db1ca236d8c6b6b0578ea4a397cd65cb811794
Aram Joseph <ajoseph@mr.preble.com>
2016-03-15 09:05:28 -0400

    Working on implementing Robbie's stuff
    ^^^

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -211,7 +211,7 @@ public class PlayerController : MonoBehaviour
 		if(damage)
 		{
 			noDamageTimer += Time.deltaTime;
-            Debug.Log("Damage");
+            //Debug.Log("Damage");
 			if(noDamageTimer > noDamage)
 			{
                 if (PlayerNumber == 1)
