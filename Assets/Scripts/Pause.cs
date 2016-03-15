0b62551100d712ce9bc866342ce5598f73e532ff
Aram Joseph <ajoseph@mr.preble.com>
2016-03-15 09:27:04 -0400

    Stuff
    ^^^

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Escape"))
+        {
+            Debug.Log("Pause");
+            Time.timeScale = 0;
+        }
+        else if (Input.GetButtonDown("Tab"))
+        {
+            Debug.Log("Unpause");
+            Time.timeScale = 1;
+        }
+    }
+}
