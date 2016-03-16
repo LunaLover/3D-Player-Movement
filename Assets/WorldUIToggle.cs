329e6df6dc41ae00f280e817892f79ea4583a2a1
Nathaniel Wilcox <nwilcox@utc4me.org>
2016-03-16 09:54:08 -0400

    Audio update

--- Assets/WorldUIToggle.cs
+++ Assets/WorldUIToggle.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class WorldUIToggle : MonoBehaviour {
 	public GameObject Toggler;
+	public GUIText Text;
 	// Use this for initialization
 	void Start () {
 		print (this.gameObject.name);
 		Toggler = this.gameObject;
+	}
+	void update(){
+		if (Toggler.name == "First")
+			Text.name = FindObjectsOfType<GUIText;
 	}
 	
 	// Update is called once per frame
